05f2700252d93ad4be39161d42aa65e167d0029e
Rob Rodi <robert.rodi@gmail.com>
2014-03-23 11:29:31 -0700

    fxcop & dispose

---
+++ RoslynRx/LogTimer.cs
@@ -0,0 +1,38 @@
+using System;
+using NLog;
+
+namespace RoslynRx
+{
+    public class LogTimer : IDisposable
+    {
+        private string message;
+        private Logger log;
+        private DateTime start = DateTime.UtcNow;
+
+        private bool disposed;
+        public LogTimer(string message, Logger log)
+        {
+            this.message = message;
+            this.log = log;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || disposed) return;
+            var ellapsed = (DateTime.UtcNow - start).TotalMilliseconds;
+            log.Info("T: {0} Took: {1} ms", message, ellapsed);
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~LogTimer()
+        {
+            Dispose(false);
+        }
+    }
+}
