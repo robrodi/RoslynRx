a89413e07530c5521fa2629cdccdda20fcc7000e
Rob Rodi <robert.rodi@gmail.com>
2014-03-22 18:52:28 -0700

    Simple Filter

--- RoslynRx.Tests/BasicRoslynRx.cs
+++ RoslynRx.Tests/BasicRoslynRx.cs
@@ -1,59 +1,38 @@
 ﻿using System;
 using System.Reactive.Linq;
+using FluentAssertions;
 using Microsoft.Reactive.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Roslyn.Scripting.CSharp;
 
 namespace RoslynRx.Tests
 {
     [TestClass]
     public class BasicRoslynRx
     {
+        public Func<EventBase, bool> filter;
+
         [TestMethod]
         public void MostBasic_Count()
         {
+            // Where type = 1
+
             var scheduler = new TestScheduler();
-            var interval = Observable.Interval(TimeSpan.FromSeconds(1), scheduler).Take(60).Select(i => new Event<long>((int)i % 2, i));
-            Func<EventBase, bool> filter = i => false;
-            interval.Subscribe(Console.WriteLine);
+            const int expectedCount = 60;
+            const int numberOfTypes = 2;
+
+            var interval = Observable.Interval(TimeSpan.FromSeconds(1), scheduler).Take(expectedCount).Select(i => new Event<long>((int)i % numberOfTypes, i));
+
+            Func<EventBase, bool> x = e => e.Type == 1;
+
+            var session = new ScriptEngine().CreateSession(this);
+            session.AddReference(this.GetType().Assembly);
+            session.Execute("filter = e => e.Type == 1;");
+
+            int count = 0;
+            interval.Where(filter).Count().Subscribe(i => count = i);
             scheduler.Start();
-            Assert.Inconclusive();
-        }
-    }
-
-    public class Event<T> : EventBase
-    {
-        public Event(DateTime time, int type, T data) : base(time, type)
-        {
-            Data = data;
-        }
-
-        public T Data { get; private set; }
-
-        public Event(int type, T data) : base(type)
-        {
-            Data = data;
-        }
-    }
-
-    public abstract class EventBase
-    {
-        public DateTime Time  { get; private set; }
-
-        public int Type { get; private set; }
-
-        protected EventBase(int type) :this(DateTime.UtcNow, type)
-        {
-        }
-
-        protected EventBase(DateTime time, int type)
-        {
-            Time = time;
-            Type = type;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("Time: {0}, Type: {1}", Time, Type);
+            count.Should().Be(expectedCount / numberOfTypes);
         }
     }
 }
