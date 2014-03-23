e1b9de43540cf06a4587796c6e17faae0a7d7e52
Rob Rodi <robert.rodi@gmail.com>
2014-03-22 20:36:40 -0700

    compiled Func

--- RoslynRx.Tests/BasicRoslynRx.cs
+++ RoslynRx.Tests/BasicRoslynRx.cs
@@ -43,6 +43,23 @@ namespace RoslynRx.Tests
             session.AddReference(typeof(Func<int, bool>).Assembly);
             var actual = session.Execute<Func<int, bool>>("i => i == 1");
             actual.Should().NotBeNull("null compilation");
+            actual.Invoke(2).Should().BeFalse("Should return false on 2");
+            actual.Invoke(1).Should().BeTrue("Should return true on 1");
+        }
+
+        [TestMethod]
+        public void Compiler_CustomType()
+        {
+            var session = new ScriptEngine().CreateSession();
+            Type generic = typeof (Func<,>);
+            var genericType = generic.MakeGenericType(typeof (EventBase), typeof (bool));
+            session.AddReference(generic.Assembly);
+            session.AddReference(typeof(EventBase).Assembly);
+            session.AddReference(genericType.Assembly);
+            var actual = session.Execute<Func<EventBase, bool>>("@event => @event.Type == 1");
+            actual.Should().NotBeNull("null compilation");
+            actual.Invoke(new Event<int>(1, 1)).Should().BeTrue();
+            actual.Invoke(new Event<int>(2, 1)).Should().BeFalse();
         }
 
         [TestMethod]
